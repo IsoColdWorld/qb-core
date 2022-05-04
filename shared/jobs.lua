abe8878e436d4d9f55e218f754343f5a5622c98a
Von <imagez9813@gmail.com>
2022-05-03 22:40:18 -0400

    Add Hooklounge

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -349,6 +349,29 @@ QBShared.Jobs = {
             },
         },
 	},
+    ['hookahlounge'] = {
+        label = 'Hookah Lounge',
+        defaultDuty = true,
+        grades = {
+            ['0'] = {
+                name = 'Employee',
+                payment = 50
+            },
+            ['1'] = {
+                name = 'Shopkeeper',
+                payment = 75
+            },
+            ['2'] = {
+                name = 'Manager',
+                payment = 100
+            },
+            ['3'] = {
+                name = 'Owner',
+                payment = 125,
+                isboss = true,
+            },
+        },
+    },
 	--[['cookies'] = {
 		label = 'Cookies',
 		defaultDuty = true,
