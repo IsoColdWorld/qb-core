7a1b85d083d65ec3945addec8c5b23f0af537a97
Von <imagez9813@gmail.com>
2022-06-11 17:39:38 -0400

    added cash as a starter item

--- shared/main.lua
+++ shared/main.lua
@@ -74,6 +74,7 @@ QBShared.StarterItems = {
     ['phone'] = { amount = 1, item = 'phone' },
     ['id_card'] = { amount = 1, item = 'id_card' },
     ['driver_license'] = { amount = 1, item = 'driver_license' },
+    ['cash'] = { amount = 5000, item = 'cash' },
 }
 
 QBShared.MaleNoGloves = {
